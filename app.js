c146f9353dd641dc1a5cf3fa7005991403bbdae3
Tanish chaurasia <tanishchaurasia56@gmail.com>
2021-10-25 18:07:40 +0530

    commited on 25/10/21

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
 const express =  require('express');
 const app =  express();
 const path = require('path');
-const axios = require('axios');
+//const axios = require('axios');
 const ejsMate = require('ejs-mate')
-let states = [];
+const cookieparser = require("cookie-parser");
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.engine('ejs',ejsMate);
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
-/* const apiData = () => {
 
-}; */
+app.use(cookieparser()); 
+
 
 
 app.get('/',(req,res)=>{
-        res.render('home');
+  res.render('home');
 })
 
 app.get('/findCenter',(req,res)=>{
-        axios.get('https://cdn-api.co-vin.in/api/v2/admin/location/states')
-        .then(function (response) {
-                states =  response.data.states.state_name;
-                console.log(states);
-        })
-        .catch(function (error) {
-                console.log(error);
-        })
-        res.render('findCenter',{states});        
+  let districtsId = req.cookies.districtsId;
+  res.render('findCenter',{districtsId});    
 })
 
+app.get('/centers',(req,res)=>{
+  let districtsId = req.cookies.districtsId;
+  res.render('centers',{districtsId});
+})
+
+app.post("/centers", (req, res) => {
+  let  districtsId  = req.body.districts;
+  res.cookie("districtsId", districtsId);
+  res.redirect("/centers");
+})
 
 app.listen(3000,() =>{
         console.log("Listening on Port 3000");
