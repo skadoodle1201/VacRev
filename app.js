9d5f67635094111c2b48f79bfabf51b1430f0f06
Tanish chaurasia <tanishchaurasia56@gmail.com>
2021-10-31 15:57:21 +0530

    committed on 31/10/21

--- app.js
+++ app.js
@@ -4,6 +4,32 @@ const path = require('path');
 const axios = require('axios');
 const ejsMate = require('ejs-mate')
 const cookieparser = require("cookie-parser");
+const mongoose = require('mongoose');
+const methodOverride =require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+const passport = require('passport');
+const User = require('./models/users');
+const LocalStrategy = require('passport-local');
+
+const dbUrl = 'mongodb://localhost:27017/vac-rev';
+
+
+mongoose.connect(dbUrl,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //useCreateIndex: true, works for older version of mongo
+    //useFindAndModify: false works for older version of mongo
+});
+
+const db = mongoose.connection;
+db.on("error",console.error.bind(console,"connection error"));
+db.once("open",()=>{
+    console.log("database connected");
+});
+
+
 
 
 app.use(express.json());
@@ -11,25 +37,54 @@ app.use(express.urlencoded({ extended: false }));
 app.engine('ejs',ejsMate);
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+app.use(methodOverride('_method'));
 
 
-app.use(cookieparser()); 
+app.use(cookieparser());
+const sessionConfig = {
+  secret: 'thisshouldbeabettersecret!',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+      httpOnly: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+}
+
+app.use(session(sessionConfig))
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  console.log(req.session)
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+})
 
 app.get('/',(req,res)=>{
   res.render('home');
 })
 
-app.get('/findCenter',(req,res)=>{
+app.get('/findingCenters/findCenter',(req,res)=>{
   let districtsId = req.cookies.districtsId;
-  res.render('findCenter',{districtsId});    
+  res.render('findingCenters/findCenter',{districtsId});    
 })
 
-app.get('/centers',(req,res)=>{
+app.get('/findingCenters/centers',(req,res)=>{
   let districtsId = req.cookies.districtsId;
-  res.render('centers',{districtsId});
+  res.render('findingCenters/centers',{districtsId});
 })
 
-app.get('/center',async(req,res)=>{
+app.get('/findingCenters/center',async(req,res)=>{
   let centerId = req.cookies.centerId;
   let d = new Date();
   let month = d.getMonth()+1;
@@ -37,20 +92,22 @@ app.get('/center',async(req,res)=>{
   let output = (day<10 ? '0' : '') + day + '-' +(month<10 ? '0' : '') + month + '-'+ d.getFullYear();
   const response = await axios.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByCenter?center_id=${centerId}&date=${output}`);
   let center = response.data.centers;
-  res.render('center',{centerId,center});
+  res.render('findingCenters/center',{centerId,center});
 })
 
-app.post("/centers", (req, res) => {
+app.post("/findingCenters/centers", (req, res) => {
   let  districtsId  = req.body.districts;
   res.cookie("districtsId", districtsId);
-  res.redirect("/centers");
+  res.redirect("/findingCenters/centers");
 })
 
-app.post("/center", (req, res) => {
+app.post("/findingCenters/center", (req, res) => {
   let  centerId  = req.body.centerId;
   res.cookie("centerId", centerId);
-  res.redirect("/center");
+  res.redirect("/findingCenters/center");
 })
+
+app.get
 
 app.listen(3000,() =>{
         console.log("Listening on Port 3000");
